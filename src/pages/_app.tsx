f14345a99af013d7f085c4f6054f7e5fc21e1152
saifulkhan <saifuletc@gmail.com>
2025-04-08 19:21:15 +0100

    cleaned

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,21 +1,6 @@
 import type { ReactNode } from "react";
 import type { NextPage } from "next";
 import type { AppProps } from "next/app";
-import { HelmetProvider, Helmet } from "react-helmet-async";
-import { CacheProvider, EmotionCache } from "@emotion/react";
-import { ThemeProvider as MuiThemeProvider, createTheme } from "@mui/material/styles";
-import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
-import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
-import createCache from "@emotion/cache";
-
-const defaultTheme = createTheme();
-
-// Create a simple emotion cache for client-side rendering
-const createEmotionCache = (): EmotionCache => {
-  return createCache({ key: 'css' });
-};
-
-const clientSideEmotionCache = createEmotionCache();
 
 type GetLayout = (page: ReactNode) => ReactNode;
 
@@ -24,32 +9,13 @@ type Page<P = {}, IP = P> = NextPage<P, IP> & {
 };
 
 type MyAppProps<P = {}> = AppProps<P> & {
-  emotionCache?: EmotionCache;
   Component: Page<P>;
 };
 
-function App({
-  Component,
-  emotionCache = clientSideEmotionCache,
-  pageProps,
-}: MyAppProps) {
+function App({ Component, pageProps }: MyAppProps) {
   const getLayout = Component.getLayout ?? ((page: ReactNode) => page);
 
-  return (
-    <CacheProvider value={emotionCache}>
-      <HelmetProvider>
-        <Helmet
-          titleTemplate="%s | Storyboard"
-          defaultTitle="Storyboard"
-        />
-        <LocalizationProvider dateAdapter={AdapterDateFns}>
-          <MuiThemeProvider theme={defaultTheme}>
-            {getLayout(<Component {...pageProps} />)}
-          </MuiThemeProvider>
-        </LocalizationProvider>
-      </HelmetProvider>
-    </CacheProvider>
-  );
+  return getLayout(<Component {...pageProps} />);
 }
 
 export default App;
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,4 @@
-import React from "react";
 import Document, { Html, Head, Main, NextScript } from "next/document";
-import createEmotionServer from "@emotion/server/create-instance";
-import createCache from "@emotion/cache";
-import { EmotionCache } from "@emotion/react";
-
-// Create a simple emotion cache for client-side rendering
-const createEmotionCache = (): EmotionCache => {
-  return createCache({ key: 'css' });
-};
 
 export default class MyDocument extends Document {
   render() {
@@ -17,14 +8,8 @@ export default class MyDocument extends Document {
           <meta charSet="utf-8" />
           <meta
             name="description"
-            content="A professional package that comes with plenty of UI components, forms, tables, charts, dashboards, pages and svg icons. Each one is fully customizable, responsive and easy to use."
+            content="A visualization component library for data storytelling"
           />
-          <meta
-            name="keywords"
-            content="mira,mui,material app,react,material,kit,dashboard,application,admin,template"
-          />
-          <meta name="author" content="Bootlab" />
-
           <link
             href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600&display=swap"
             rel="stylesheet"
@@ -38,68 +23,3 @@ export default class MyDocument extends Document {
     );
   }
 }
-
-// `getInitialProps` belongs to `_document` (instead of `_app`),
-// it's compatible with static-site generation (SSG).
-MyDocument.getInitialProps = async (ctx) => {
-  // Resolution order
-  //
-  // On the server:
-  // 1. app.getInitialProps
-  // 2. page.getInitialProps
-  // 3. document.getInitialProps
-  // 4. app.render
-  // 5. page.render
-  // 6. document.render
-  //
-  // On the server with error:
-  // 1. document.getInitialProps
-  // 2. app.render
-  // 3. page.render
-  // 4. document.render
-  //
-  // On the client
-  // 1. app.getInitialProps
-  // 2. page.getInitialProps
-  // 3. app.render
-  // 4. page.render
-
-  const originalRenderPage = ctx.renderPage;
-
-  // You can consider sharing the same emotion cache between all the SSR requests to speed up performance.
-  // However, be aware that it can have global side effects.
-  const cache = createEmotionCache();
-  const { extractCriticalToChunks } = createEmotionServer(cache);
-
-  /* eslint-disable */
-  ctx.renderPage = () =>
-    originalRenderPage({
-      enhanceApp: (App: any) =>
-        function EnhanceApp(props) {
-          return <App emotionCache={cache} {...props} />;
-        },
-    });
-  /* eslint-enable */
-
-  const initialProps = await Document.getInitialProps(ctx);
-  // This is important. It prevents emotion to render invalid HTML.
-  // See https://github.com/mui-org/material-ui/issues/26561#issuecomment-855286153
-  const emotionStyles = extractCriticalToChunks(initialProps.html);
-  const emotionStyleTags = emotionStyles.styles.map((style) => (
-    <style
-      data-emotion={`${style.key} ${style.ids.join(" ")}`}
-      key={style.key}
-      // eslint-disable-next-line react/no-danger
-      dangerouslySetInnerHTML={{ __html: style.css }}
-    />
-  ));
-
-  return {
-    ...initialProps,
-    // Styles fragment is rendered after the app and page rendering finish.
-    styles: [
-      ...React.Children.toArray(initialProps.styles),
-      ...emotionStyleTags,
-    ],
-  };
-};
